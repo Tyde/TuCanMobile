8b7264c278d5142e0e6f3c5843296f1d6b3370c7
Daniel Thiem <info@daniel-thiem.de>
2012-10-07 15:57:09 +0200

    Session checking in ModuleScraper

--- src/com/dalthed/tucan/scraper/ModuleScraper.java
+++ src/com/dalthed/tucan/scraper/ModuleScraper.java
@@ -17,29 +17,41 @@ import com.dalthed.tucan.exceptions.TucanDownException;
 public class ModuleScraper extends BasicScraper {
 	public ArrayList<String> eventLinks;
 	public String title;
-	
+
 	public ModuleScraper(Context context, AnswerObject result) {
 		super(context, result);
 	}
 
 	@Override
 	public ListAdapter scrapeAdapter(int mode) throws LostSessionException, TucanDownException {
+		if (checkForLostSeesion()) {
+			return scrapeModule();
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Extracts the Module Information from the HTML code
+	 * @return ListAdapter for the Activity
+	 */
+	private ListAdapter scrapeModule() {
 		title = doc.select("h1").text();
 		Elements events = doc.select("a[name=eventLink]");
-		//System.out.println(events);
+		// System.out.println(events);
 		ArrayList<String> eventNames = new ArrayList<String>();
-						  eventLinks = new ArrayList<String>();
-		if(events.size()%3==0){
+		eventLinks = new ArrayList<String>();
+		if (events.size() % 3 == 0) {
 			for (int i = 0; i < events.size(); i += 3) {
-				eventNames.add(events.get(i).text()+" "+events.get(i+1).text()+" "+events.get(i+2).text());
+				eventNames.add(events.get(i).text() + " " + events.get(i + 1).text() + " "
+						+ events.get(i + 2).text());
 				eventLinks.add(events.get(i).attr("href"));
 				System.out.println(events.get(i).attr("href"));
 			}
 		}
-		
-		
-		ArrayAdapter<String> simpleAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1	,eventNames);
+
+		ArrayAdapter<String> simpleAdapter = new ArrayAdapter<String>(context,
+				android.R.layout.simple_list_item_1, eventNames);
 		return simpleAdapter;
 	}
-
 }
