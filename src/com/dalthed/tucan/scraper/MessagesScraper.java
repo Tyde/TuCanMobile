403aa8895fe4babee840199a62b18ade8cb0b861
Daniel Thiem <info@daniel-thiem.de>
2012-10-07 16:11:50 +0200

    Messages Scrape plus Orientation fix

---
+++ src/com/dalthed/tucan/scraper/MessagesScraper.java
@@ -0,0 +1,86 @@
+package com.dalthed.tucan.scraper;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import android.content.Context;
+import android.widget.ListAdapter;
+
+import com.dalthed.tucan.TucanMobile;
+import com.dalthed.tucan.Connection.AnswerObject;
+import com.dalthed.tucan.Connection.BrowserAnswerReciever;
+import com.dalthed.tucan.Connection.CookieManager;
+import com.dalthed.tucan.Connection.RequestObject;
+import com.dalthed.tucan.Connection.SimpleSecureBrowser;
+import com.dalthed.tucan.adapters.ThreeLinesAdapter;
+import com.dalthed.tucan.exceptions.LostSessionException;
+import com.dalthed.tucan.exceptions.TucanDownException;
+
+
+public class MessagesScraper extends BasicScraper {
+	
+	private CookieManager localCookieManager;
+	private int step=0;
+	public ArrayList<String> messageLink;
+	public MessagesScraper(Context context, AnswerObject result) {
+		super(context, result);
+		localCookieManager = result.getCookieManager();
+	}
+
+	@Override
+	public ListAdapter scrapeAdapter(int mode) throws LostSessionException, TucanDownException {
+		if (checkForLostSeesion()) {
+			if (step == 0) {
+				loadArchive();
+			} else {
+				return extractMessageList();
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 */
+	private ListAdapter extractMessageList() {
+		Iterator<Element> messageRows = doc.select("tr.tbdata").iterator();
+		ArrayList<String> messageDate = new ArrayList<String>();
+		ArrayList<String> messageAuthor = new ArrayList<String>();
+		ArrayList<String> messageTitle = new ArrayList<String>();
+		messageLink = new ArrayList<String>();
+
+		while (messageRows.hasNext()) {
+			Element next = messageRows.next();
+			Elements MessageCols = next.select("td");
+			if (MessageCols.size() > 0) {
+				messageDate.add(MessageCols.get(1).text() + " - "
+						+ MessageCols.get(2).text());
+				messageAuthor.add("Von: " + MessageCols.get(3).text());
+				messageTitle.add(MessageCols.get(4).text());
+				messageLink.add(MessageCols.get(4).select("a").attr("href"));
+			}
+		}
+		ThreeLinesAdapter listAdapter = new ThreeLinesAdapter(context,messageTitle, messageDate,
+				messageAuthor);
+		return listAdapter;
+	}
+
+	/**
+	 * 
+	 */
+	private void loadArchive() {
+		String AllMailLink = TucanMobile.TUCAN_PROT + TucanMobile.TUCAN_HOST
+				+ doc.select("div.tbcontrol").select("a").last().attr("href");
+		step = 1;
+		if(context instanceof BrowserAnswerReciever){
+			SimpleSecureBrowser callOverviewBrowser = new SimpleSecureBrowser((BrowserAnswerReciever) context);
+			RequestObject thisRequest = new RequestObject(AllMailLink, localCookieManager,
+					RequestObject.METHOD_GET, "");
+			callOverviewBrowser.execute(thisRequest);
+		}
+	}
+
+}
--- src/com/dalthed/tucan/ui/Messages.java
+++ src/com/dalthed/tucan/ui/Messages.java
@@ -1,6 +1,5 @@
 package com.dalthed.tucan.ui;
 
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -14,6 +13,7 @@ import org.jsoup.select.Elements;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.renderscript.Script;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,20 +29,25 @@ import com.dalthed.tucan.Connection.AnswerObject;
 import com.dalthed.tucan.Connection.CookieManager;
 import com.dalthed.tucan.Connection.RequestObject;
 import com.dalthed.tucan.Connection.SimpleSecureBrowser;
+import com.dalthed.tucan.exceptions.LostSessionException;
+import com.dalthed.tucan.exceptions.TucanDownException;
+import com.dalthed.tucan.scraper.BasicScraper;
+import com.dalthed.tucan.scraper.MessagesScraper;
+import com.dalthed.tucan.scraper.RegisterExamsScraper;
 import com.dalthed.tucan.util.ConfigurationChangeStorage;
 
 public class Messages extends SimpleWebListActivity {
 	private String UserName;
 	private CookieManager localCookieManager;
 	private static final String LOG_TAG = "TuCanMobile";
-	private String  URLStringtoCall;
-	private int mode=0;
-	private ArrayList<String> MessageLink;
+	private String URLStringtoCall;
+	private MessagesScraper scrape;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState,true,4);
+		super.onCreate(savedInstanceState, true, 4);
 		setContentView(R.layout.messages);
-		BugSenseHandler.setup(this,"ed5c1682");
+		BugSenseHandler.setup(this, "ed5c1682");
 		String CookieHTTPString = getIntent().getExtras().getString("Cookie");
 		URLStringtoCall = getIntent().getExtras().getString("URL");
 		UserName = getIntent().getExtras().getString("UserName");
@@ -51,103 +56,66 @@ public class Messages extends SimpleWebListActivity {
 		try {
 			URLtoCall = new URL(URLStringtoCall);
 			localCookieManager = new CookieManager();
-			localCookieManager.generateManagerfromHTTPString(
-					URLtoCall.getHost(), CookieHTTPString);
-			callResultBrowser = new SimpleSecureBrowser(
-					this);
-			RequestObject thisRequest = new RequestObject(URLStringtoCall,
-					localCookieManager, RequestObject.METHOD_GET, "");
+			localCookieManager.generateManagerfromHTTPString(URLtoCall.getHost(), CookieHTTPString);
+			callResultBrowser = new SimpleSecureBrowser(this);
+			RequestObject thisRequest = new RequestObject(URLStringtoCall, localCookieManager,
+					RequestObject.METHOD_GET, "");
 
 			callResultBrowser.execute(thisRequest);
 		} catch (MalformedURLException e) {
 			Log.e(LOG_TAG, e.getMessage());
 		}
 	}
+
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 		setContentView(R.layout.messages);
 	}
 
-	class MessageAdapter extends ArrayAdapter<String>{
-
-		ArrayList<String> messageDate,messageAuthor;
-		public MessageAdapter(ArrayList<String> messageTitle,ArrayList<String> messageDate,ArrayList<String> messageAuthor) {
-			super(Messages.this,R.layout.row_vv_events,R.id.row_vv_veranst,messageTitle);
-			this.messageAuthor=messageAuthor;
-			this.messageDate=messageDate;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View row = super.getView(position, convertView, parent);
-			TextView TypeTextView = (TextView) row.findViewById(R.id.row_vv_type);
-			TextView DozentTextView = (TextView) row.findViewById(R.id.row_vv_dozent);
-			
-			TypeTextView.setText(messageDate.get(position));
-			DozentTextView.setText(messageAuthor.get(position));
-			
-			return row;
-		}
-		
-	}
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-		Intent MessageStartIntent = new Intent(Messages.this, SingleMessage.class);
-		MessageStartIntent.putExtra("URL", TucanMobile.TUCAN_PROT + TucanMobile.TUCAN_HOST + MessageLink.get(position));
-		MessageStartIntent.putExtra("User", UserName);
-		MessageStartIntent.putExtra("Cookie", localCookieManager
-				.getCookieHTTPString(TucanMobile.TUCAN_HOST));
-		startActivity(MessageStartIntent);
+		if (scrape != null && scrape.messageLink.size()>position) {
+			Intent MessageStartIntent = new Intent(Messages.this, SingleMessage.class);
+			MessageStartIntent.putExtra("URL", TucanMobile.TUCAN_PROT + TucanMobile.TUCAN_HOST
+					+ scrape.messageLink.get(position));
+			MessageStartIntent.putExtra("User", UserName);
+			MessageStartIntent.putExtra("Cookie",
+					localCookieManager.getCookieHTTPString(TucanMobile.TUCAN_HOST));
+			startActivity(MessageStartIntent);
+		}
 	}
-	
+
 	public void onPostExecute(AnswerObject result) {
-		Document doc = Jsoup.parse(result.getHTML());
-		sendHTMLatBug(result.getHTML());
-		if(result.getHTML().length()<10 || doc.select("span.notLoggedText").text().length()>0){
-			Intent BackToLoginIntent = new Intent(this,TuCanMobileActivity.class);
+		scrape = new MessagesScraper(this, result);
+		try {
+			setListAdapter(scrape.scrapeAdapter(0));
+		} catch (LostSessionException e) {
+			Intent BackToLoginIntent = new Intent(this, TuCanMobileActivity.class);
 			BackToLoginIntent.putExtra("lostSession", true);
 			startActivity(BackToLoginIntent);
+		} catch (TucanDownException e) {
+			TucanMobile.alertOnTucanDown(this, e.getMessage());
 		}
-		else {
-			if(mode==0){
-				String AllMailLink = TucanMobile.TUCAN_PROT + TucanMobile.TUCAN_HOST + doc.select("div.tbcontrol").select("a").last().attr("href");
-				mode=1;
-				SimpleSecureBrowser callOverviewBrowser = new SimpleSecureBrowser(
-						this);
-				RequestObject thisRequest = new RequestObject(AllMailLink,
-						localCookieManager, RequestObject.METHOD_GET, "");
-				callOverviewBrowser.execute(thisRequest);
-			}
-			else {
-				Iterator<Element> MessageRows = doc.select("tr.tbdata").iterator();
-				ArrayList<String> MessageDate = new ArrayList<String>();
-				ArrayList<String> MessageAuthor = new ArrayList<String>();
-				ArrayList<String> MessageTitle = new ArrayList<String>();
-				MessageLink = new ArrayList<String>();
-				
-				while(MessageRows.hasNext()){
-					Element next = MessageRows.next();
-					Elements MessageCols = next.select("td");
-					if(MessageCols.size()>0){
-						MessageDate.add(MessageCols.get(1).text()+" - "+MessageCols.get(2).text());
-						MessageAuthor.add("Von: "+MessageCols.get(3).text());
-						MessageTitle.add(MessageCols.get(4).text());
-						MessageLink.add(MessageCols.get(4).select("a").attr("href"));
-					}			
-				}
-				MessageAdapter ListAdapter = new MessageAdapter(MessageTitle, MessageDate, MessageAuthor);
-				setListAdapter(ListAdapter);
-			}
-		}
+
 	}
+
 	@Override
 	public ConfigurationChangeStorage saveConfiguration() {
-		return null;
+		ConfigurationChangeStorage cStore = new ConfigurationChangeStorage();
+		cStore.adapters.add(getListAdapter());
+		cStore.scrapers.add(scrape);
+		return cStore;
 	}
+
 	@Override
 	public void retainConfiguration(ConfigurationChangeStorage conf) {
+		setListAdapter(conf.adapters.get(0));
+		BasicScraper retainedScraper = conf.scrapers.get(0);
+		if (retainedScraper != null && retainedScraper instanceof MessagesScraper) {
+			scrape = (MessagesScraper) retainedScraper;
+		}
 	}
 
 }
