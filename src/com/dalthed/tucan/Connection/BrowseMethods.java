85464d8153f6ef0e7be94c321aee3e33fa792b27
Daniel Thiem <info@daniel-thiem.de>
2012-10-28 17:22:02 +0100

    BrowseMethods clean Up

--- src/com/dalthed/tucan/Connection/BrowseMethods.java
+++ src/com/dalthed/tucan/Connection/BrowseMethods.java
@@ -7,113 +7,139 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
-import java.util.zip.GZIPInputStream;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import org.acra.ErrorReporter;
+
 import android.os.Build;
 import android.util.Log;
+
+import com.dalthed.tucan.TucanMobile;
+
 /**
- * BrowseMethods implementiert wichtige Methoden, um Webseiten angepasst zu Laden
+ * BrowseMethods implementiert wichtige Methoden, um Webseiten angepasst zu
+ * Laden
+ * 
  * @author Tyde
- *
+ * 
  */
 public class BrowseMethods {
 	private HttpURLConnection HTTPConnection;
 	private CookieManager myCookies;
-	public boolean iwantthereader=false;
+	public boolean iwantthereader = false;
 	public InputStream in;
 	public InputStreamReader isr;
 	private static final String LOG_TAG = "TuCanMobile";
 	public boolean HTTPS = true;
-	private static final int MAX_BUFFER_SIZE = 1024;
+	
+
 	/**
 	 * setImportantHeaders setzt Header für den HTTP-Request
-	 * @param RequestMethod RequestObject.METHOD_POST oder RequestObject.METHOD_GET
-	 * @param domain wichtig für CookieHandler
+	 * 
+	 * @param RequestMethod
+	 *            RequestObject.METHOD_POST oder RequestObject.METHOD_GET
+	 * @param domain
+	 *            wichtig für CookieHandler
 	 */
 	private void setImportantHeaders(String RequestMethod, String domain) {
 		try {
+			// GET oder POST setzen
 			HTTPConnection.setRequestMethod(RequestMethod);
 		} catch (ProtocolException e) {
 			e.printStackTrace();
 		}
-		HTTPConnection.setRequestProperty("User-Agent",
-		 "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:7.0.1) Gecko/20100101 Firefox/7.0.1");
+		// Browser-Header: Currently Chrome 22
+		HTTPConnection
+				.setRequestProperty(
+						"User-Agent",
+						"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4");
 		HTTPConnection.setInstanceFollowRedirects(false);
+		// Cookies setzen:
 		if (myCookies.domain_exists(domain)) {
-			Log.i(LOG_TAG, "Cookie gesetzt:" +myCookies.getCookieHTTPString(domain));
-			HTTPConnection.setRequestProperty("Cookie",
-					myCookies.getCookieHTTPString(domain));
+			if (TucanMobile.DEBUG) {
+				Log.i(LOG_TAG, "Cookie gesetzt:" + myCookies.getCookieHTTPString(domain));
+			}
+			HTTPConnection.setRequestProperty("Cookie", myCookies.getCookieHTTPString(domain));
 		}
 	}
+
 	/**
-	 * HauptMethode der BrowseMethods: lädt die Seite und gibt sie als AnswerObject wieder zurück
-	 * @param requestInfo RequestObject mit allen Wichtigen Informationen zum Request
+	 * HauptMethode der BrowseMethods: lädt die Seite und gibt sie als
+	 * AnswerObject wieder zurück
+	 * 
+	 * @param requestInfo
+	 *            RequestObject mit allen Wichtigen Informationen zum Request
 	 * @return AnswerObject der Seite
 	 */
 	public AnswerObject browse(RequestObject requestInfo) {
 		String redirectURL = "";
 		String alllines = "";
 		if (Integer.parseInt(Build.VERSION.SDK) < 9) {
-	        System.setProperty("http.keepAlive", "false");
-	    }
+			// Notwendig, da sonst die Verbindung bei älteren Systemen nicht
+			// klappt
+			System.setProperty("http.keepAlive", "false");
+		}
 		try {
+			// Informationen aus RequestObject auslesen
 			URL realURL = requestInfo.getmyURL();
 			myCookies = requestInfo.getCookies();
 			String RequestMethod = requestInfo.getMethod();
 			String postdata = requestInfo.getPostData();
-			if(HTTPS==true){
+
+			if (HTTPS == true) {
 				HTTPConnection = (HttpsURLConnection) realURL.openConnection();
-			}
-			else {
+			} else {
 				HTTPConnection = (HttpURLConnection) realURL.openConnection();
 			}
-			
-			Log.i(LOG_TAG, "Started Connection with: " + realURL.toString());
-			if(RequestMethod == "POST")
-				HTTPConnection.setDoOutput(true);
-			
-			
-
-			setImportantHeaders(RequestMethod, realURL.getHost());
+			if (TucanMobile.DEBUG) {
+				Log.i(LOG_TAG, "Started Connection with: " + realURL.toString());
+			}
 			if (RequestMethod == "POST") {
+				// Output bei POST-Übertragung aktivierten
+				HTTPConnection.setDoOutput(true);
+			}
+			// Ursprünglichen Request setzen
+			setImportantHeaders(RequestMethod, realURL.getHost());
 
-				OutputStreamWriter out = new OutputStreamWriter(
-						HTTPConnection.getOutputStream());
+			if (RequestMethod == "POST") {
+				// Post-Daten senden
+				OutputStreamWriter out = new OutputStreamWriter(HTTPConnection.getOutputStream());
 				out.write(postdata);
 				out.close();
 			}
 
-			/*
-			GZIPInputStream in = (GZIPInputStream) HTTPConnection
-					.getInputStream();*/
+			// Antwort auslesen
 			in = HTTPConnection.getInputStream();
 			isr = new InputStreamReader(in, "ISO8859_1");
-			
-			if(iwantthereader==false){
-				
-				BufferedReader bin = new BufferedReader(isr,8*1024);
+
+			if (iwantthereader == false) {
+
+				BufferedReader bin = new BufferedReader(isr, 8 * 1024);
+				// Header auslesen
 				for (int n = 0;; n++) {
 					String headerValue = HTTPConnection.getHeaderField(n);
 					String headerName = HTTPConnection.getHeaderFieldKey(n);
 					if (headerValue == null && headerName == null) {
+
 						break;
 					}
 
+					// Cookies auslesen
 					if ("Set-Cookie".equalsIgnoreCase(headerName)) {
-						Log.i(LOG_TAG, "Lese Cookies aus");
+						if (TucanMobile.DEBUG) {
+							Log.i(LOG_TAG, "Lese Cookies aus");
+						}
 						String[] multipleCookies = headerValue.split(";\\s*");
 						for (String ccy : multipleCookies) {
 							String[] eachVal = ccy.split("=");
 							if (eachVal.length == 2)
-								myCookies.inputCookie(realURL.getHost(),
-										eachVal[0], eachVal[1]);
+								myCookies.inputCookie(realURL.getHost(), eachVal[0], eachVal[1]);
 							else
-								myCookies.inputCookie(realURL.getHost(),
-										eachVal[0], null);
+								myCookies.inputCookie(realURL.getHost(), eachVal[0], null);
 						}
 					}
+					// Eventuellen redirect auslesen und speichern
 					if ("refresh".equalsIgnoreCase(headerName)) {
 						String[] getredirectURL = headerValue.split("URL=");
 						redirectURL = getredirectURL[1];
@@ -121,26 +147,32 @@ public class BrowseMethods {
 
 				}
 				int contentlength = HTTPConnection.getContentLength();
-				Log.i(LOG_TAG, contentlength + "...");
+				if (TucanMobile.DEBUG) {
+					Log.i(LOG_TAG, contentlength + "...");
+				}
+				
+				//Server-Antwort auslesen und speichern
 				StringBuilder inputBuilder = new StringBuilder();
 				String inputLine;
-				int read =0;
+				
 				while ((inputLine = bin.readLine()) != null) {
 
 					inputBuilder.append(inputLine);
+
 					
-					read += inputLine.getBytes("ISO8859_1").length+2;
-				
+
 				}
 				in.close();
 				alllines = inputBuilder.toString();
 			}
-			
+
 		} catch (Exception e) {
+			ErrorReporter.getInstance().handleSilentException(e);
+			if(TucanMobile.DEBUG){
+				e.printStackTrace();
+			}
 			
-			e.printStackTrace();
 		}
-		return new AnswerObject(alllines, redirectURL, myCookies, requestInfo
-				.getmyURL().toString());
+		return new AnswerObject(alllines, redirectURL, myCookies, requestInfo.getmyURL().toString());
 	}
 }
