c28e94dbe3816bdaef8ec8988ad134352fbcdd77
Daniel Thiem <info@daniel-thiem.de>
2012-10-28 16:53:12 +0100

    Javadoc RequestObject

--- src/com/dalthed/tucan/Connection/RequestObject.java
+++ src/com/dalthed/tucan/Connection/RequestObject.java
@@ -5,60 +5,130 @@ import java.net.URL;
 
 import android.util.Log;
 
+/**
+ * Objekt, welches alle notwendigen Informationen für einen <code>HTTP</code>/
+ * <code>HTTPS</code>-Request enthält
+ * 
+ * @author Tyde
+ * 
+ */
 public class RequestObject {
 	private URL RequestURL;
 	private CookieManager RequestCookies;
 	private String RequestMethod;
 	private String RequestpostData;
-	
-	final public static String METHOD_GET="GET";
-	final public static String METHOD_POST="POST";
-	
+	/**
+	 * HTTP-Methode GET
+	 */
+	final public static String METHOD_GET = "GET";
+	/**
+	 * HTTP-Methode POST
+	 */
+	final public static String METHOD_POST = "POST";
+
 	private static final String LOG_TAG = "TuCanMobile";
-	
-	public RequestObject(String RequestString,CookieManager RequestCookiemanager,String method,String postdata)  {
+
+	/**
+	 * Objekt, welches alle notwendigen Informationen für einen
+	 * <code>HTTP</code>/<code>HTTPS</code>-Request enthält
+	 * 
+	 * @param RequestString
+	 *            URL(als String), welche aufgerufen werden soll
+	 * @param RequestCookiemanager
+	 *            {@link CookieManager}, welcher die notwendigen Cookies
+	 *            enthalten soll
+	 * @param method
+	 *            Entweder {@link #METHOD_GET} oder {@link #METHOD_POST}
+	 * @param postdata
+	 *            Daten, welche bei {@link #METHOD_POST} mitgesendert werden
+	 *            können
+	 * @author Daniel Thiem
+	 */
+	public RequestObject(String RequestString, CookieManager RequestCookiemanager, String method,
+			String postdata) {
 		try {
-			this.RequestURL=new URL(RequestString);
-			//Log.i(LOG_TAG,"Hier haste die URL:"+RequestString);
+			this.RequestURL = new URL(RequestString);
+			// Log.i(LOG_TAG,"Hier haste die URL:"+RequestString);
 		} catch (MalformedURLException e) {
-			
-			Log.e(LOG_TAG,"Malfomed URL");
+
+			Log.e(LOG_TAG, "Malfomed URL");
 		}
-		this.RequestCookies=RequestCookiemanager;
-		if(method==METHOD_GET){
-			this.RequestMethod=METHOD_GET;
+		this.RequestCookies = RequestCookiemanager;
+		if (method == METHOD_GET) {
+			this.RequestMethod = METHOD_GET;
+		} else if (method == METHOD_POST) {
+			this.RequestMethod = METHOD_POST;
+		} else {
+			this.RequestMethod = METHOD_GET;
 		}
-		else if(method==METHOD_POST){
-			this.RequestMethod=METHOD_POST;
-		}
-		else {
-			this.RequestMethod=METHOD_GET;
-		}
-		this.RequestpostData=postdata;
+		this.RequestpostData = postdata;
 	}
-	
-	public RequestObject(String RequestString,String method,String postdata) {
-		this(RequestString,new CookieManager(),method,postdata);
+
+	/**
+	 * Objekt, welches alle notwendigen Informationen für einen
+	 * <code>HTTP</code>/<code>HTTPS</code>-Request enthält. <br>
+	 * <br>
+	 * Dieser Konstruktor sollte nur aufgerufen werden, falls noch keine Cookies
+	 * vorhanden sind.
+	 * 
+	 * @param RequestString
+	 *            URL(als String), welche aufgerufen werden soll
+	 * @param method
+	 *            Entweder {@link #METHOD_GET} oder {@link #METHOD_POST}
+	 * @param postdata
+	 *            Daten, welche bei {@link #METHOD_POST} mitgesendert werden
+	 *            können
+	 * @author Daniel Thiem
+	 */
+	public RequestObject(String RequestString, String method, String postdata) {
+		this(RequestString, new CookieManager(), method, postdata);
 	}
-	
+
+	/**
+	 * 
+	 * @return aufzurufende URL
+	 * @author Daniel Thiem
+	 */
 	public URL getmyURL() {
-		return this.RequestURL;		
+		return this.RequestURL;
 	}
-	
+
+	/**
+	 * 
+	 * @param newManager
+	 *            neuer {@link CookieManager}
+	 * 
+	 * @author Daniel Thiem
+	 */
 	public void setCookieManager(CookieManager newManager) {
-		this.RequestCookies=newManager;
+		this.RequestCookies = newManager;
 	}
-	
+
+	/**
+	 * 
+	 * @return {@link CookieManager}
+	 * @author Daniel Thiem
+	 */
 	public CookieManager getCookies() {
 		return this.RequestCookies;
 	}
-	
+
+	/**
+	 * 
+	 * @return post Data
+	 * @author Daniel Thiem
+	 */
 	public String getPostData() {
 		return this.RequestpostData;
 	}
-	
+
+	/**
+	 * 
+	 * @return genutzte Methode
+	 * @author Daniel Thiem
+	 */
 	public String getMethod() {
 		return this.RequestMethod;
 	}
-	
+
 }
