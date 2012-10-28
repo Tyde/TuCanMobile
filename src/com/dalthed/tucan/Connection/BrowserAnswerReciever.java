74afb534d5c45c086ca0c933b600a7ed19877950
Daniel Thiem <info@daniel-thiem.de>
2012-10-28 17:02:39 +0100

    Javadoc BrowserAnswerReciever

--- src/com/dalthed/tucan/Connection/BrowserAnswerReciever.java
+++ src/com/dalthed/tucan/Connection/BrowserAnswerReciever.java
@@ -1,7 +1,15 @@
 package com.dalthed.tucan.Connection;
 
-import com.dalthed.tucan.util.ConfigurationChangeStorage;
+import android.app.Activity;
+import android.widget.ListAdapter;
 
+import com.dalthed.tucan.scraper.BasicScraper;
+import com.dalthed.tucan.util.ConfigurationChangeStorage;
+/**
+ * Interface, das es ermöglicht, die empfangenen Antworten vom Server innerhalb der {@link Activity} weiterzuverarbeiten
+ * @author Daniel Thiem
+ *
+ */
 public interface BrowserAnswerReciever {
 	/**
 	 * Wird aufgerufen, wenn SimpleSecureBrowser fertig ist.
@@ -11,14 +19,15 @@ public interface BrowserAnswerReciever {
 	public abstract void onPostExecute(AnswerObject result);
 	
 	/**
+	 * Gibt ein {@link ConfigurationChangeStorage} zurück welche alle wichtigen {@link BasicScraper}, {@link ListAdapter} und {@link SimpleSecureBrowser} der Activity enthält
+	 * @return gefüllter {@link ConfigurationChangeStorage}
 	 * 
-	 * @return
 	 */
 	public abstract ConfigurationChangeStorage saveConfiguration();
 	
 	/**
-	 * 
-	 * @param conf
+	 * Pflegt den empfangenen {@link ConfigurationChangeStorage} wieder in die {@link Activity} ein 
+	 * @param conf {@link ConfigurationChangeStorage}, welcher relevante Daten für die Activity enthält
 	 */
 	public abstract void retainConfiguration(ConfigurationChangeStorage conf);
 }
