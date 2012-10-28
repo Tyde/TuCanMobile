850320b1d1afd7981cf521a0a84497cc6967a887
Daniel Thiem <info@daniel-thiem.de>
2012-10-28 17:23:27 +0100

    BackgroundBrowserReciever Javadoc

--- src/com/dalthed/tucan/Connection/BackgroundBrowserReciever.java
+++ src/com/dalthed/tucan/Connection/BackgroundBrowserReciever.java
@@ -1,8 +1,16 @@
 package com.dalthed.tucan.Connection;
 
+/**
+ * Wie {@link BrowserAnswerReciever}, nur für {@link SimpleBackgroundBrowser}. Derzeit noch nicht voll funktionsfähig
+ * @author Daniel Thiem
+ *
+ */
 public interface BackgroundBrowserReciever {
 	
-	
+	/**
+	 * Wird aufgerufen, wenn der Request vollständig ausgelesen wurde
+	 * @param result Antwort des Servers
+	 */
 	public void onBackgroundBrowserFinalized(AnswerObject result);
 	
 	public boolean getwindowFeatureCalled();
