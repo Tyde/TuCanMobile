396044bfbd68e270ad4857a9b21849113d3725ab
Daniel Thiem <info@daniel-thiem.de>
2015-07-23 20:58:02 +0200

    Warning before experimental RegisterExams

--- src/com/dalthed/tucan/ui/RegisterExams.java
+++ src/com/dalthed/tucan/ui/RegisterExams.java
@@ -20,6 +20,7 @@ package com.dalthed.tucan.ui;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -40,107 +41,113 @@ import com.dalthed.tucan.scraper.RegisterExamsScraper;
 import com.dalthed.tucan.util.ConfigurationChangeStorage;
 
 public class RegisterExams extends SimpleWebListActivity {
-	private String  URLStringtoCall;
-	private CookieManager localCookieManager;
+    private String URLStringtoCall;
+    private CookieManager localCookieManager;
 
-	private static final String LOG_TAG = "TuCanMobile";
+    private static final String LOG_TAG = "TuCanMobile";
 
-	int mode = 0;
-	private RegisterExamsScraper scrape;
+    int mode = 0;
+    private RegisterExamsScraper scrape;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.exams);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exams);
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle(getString(R.string.register_exams_alert_title))
+                .setMessage(getString(R.string.register_exams_alert_message))
+                .setPositiveButton(getString(R.string.register_exams_alert_accept),null)
+                .show();
 
-		// Webhandling Start
-		String CookieHTTPString = getIntent().getExtras().getString("Cookie");
-		URLStringtoCall = getIntent().getExtras().getString("URL");
-		
-		URL URLtoCall;
-		if (!restoreResultBrowser()) {
-			try {
-				URLtoCall = new URL(URLStringtoCall);
+        // Webhandling Start
+        String CookieHTTPString = getIntent().getExtras().getString("Cookie");
+        URLStringtoCall = getIntent().getExtras().getString("URL");
 
-				localCookieManager = new CookieManager();
-				localCookieManager.generateManagerfromHTTPString(URLtoCall.getHost(),
-						CookieHTTPString);
-				callResultBrowser = new SimpleSecureBrowser(this);
-				if (TucanMobile.DEBUG) {
-					callResultBrowser.HTTPS = this.HTTPS;
-				}
-				RequestObject thisRequest = new RequestObject(URLStringtoCall, localCookieManager,
-						RequestObject.METHOD_GET, "");
+        URL URLtoCall;
+        if (!restoreResultBrowser()) {
+            try {
+                URLtoCall = new URL(URLStringtoCall);
 
-				callResultBrowser.execute(thisRequest);
-			} catch (MalformedURLException e) {
-				Log.e(LOG_TAG, e.getMessage());
-			}
-		}
+                localCookieManager = new CookieManager();
+                localCookieManager.generateManagerfromHTTPString(URLtoCall.getHost(),
+                        CookieHTTPString);
+                callResultBrowser = new SimpleSecureBrowser(this);
+                if (TucanMobile.DEBUG) {
+                    callResultBrowser.HTTPS = this.HTTPS;
+                }
+                RequestObject thisRequest = new RequestObject(URLStringtoCall, localCookieManager,
+                        RequestObject.METHOD_GET, "");
 
-	}
+                callResultBrowser.execute(thisRequest);
+            } catch (MalformedURLException e) {
+                Log.e(LOG_TAG, e.getMessage());
+            }
+        }
 
-	public void onPostExecute(AnswerObject result) {
-		if (scrape == null) {
-			scrape = new RegisterExamsScraper(this, result, URLStringtoCall, localCookieManager);
-		} else {
-			scrape.setNewAnswer(result);
-		}
-		try {
-			switch (mode) {
-			case 0:
-				setListAdapter(scrape.scrapeAdapter(mode));
-				break;
+    }
 
-			case 1:
-				mode = scrape.getRegisterdialog();
-				break;
-			case 3:
-				mode = scrape.startRegistration();
-				break;
+    public void onPostExecute(AnswerObject result) {
+        if (scrape == null) {
+            scrape = new RegisterExamsScraper(this, result, URLStringtoCall, localCookieManager);
+        } else {
+            scrape.setNewAnswer(result);
+        }
+        try {
+            switch (mode) {
+                case 0:
+                    setListAdapter(scrape.scrapeAdapter(mode));
+                    break;
 
-			}
+                case 1:
+                    mode = scrape.getRegisterdialog();
+                    break;
+                case 3:
+                    mode = scrape.startRegistration();
+                    break;
 
-		} catch (LostSessionException e) {
-			Intent BackToLoginIntent = new Intent(this, TuCanMobileActivity.class);
-			BackToLoginIntent.putExtra("lostSession", true);
-			startActivity(BackToLoginIntent);
-		} catch (TucanDownException e) {
-			TucanMobile.alertOnTucanDown(this, e.getMessage());
-		}
+            }
 
-	}
+        } catch (LostSessionException e) {
+            Intent BackToLoginIntent = new Intent(this, TuCanMobileActivity.class);
+            BackToLoginIntent.putExtra("lostSession", true);
+            startActivity(BackToLoginIntent);
+        } catch (TucanDownException e) {
+            TucanMobile.alertOnTucanDown(this, e.getMessage());
+        }
 
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		if (scrape.examSelection.get(position) == 2 || scrape.examSelection.get(position) == 3) {
-			if (scrape.examSelection.get(position) == 2)
-				mode = 1;
-			else if (scrape.examSelection.get(position) == 3)
-				mode = 1;
-			SimpleSecureBrowser callOverviewBrowser = new SimpleSecureBrowser(this);
-			RequestObject callstatuschange = new RequestObject(TucanMobile.TUCAN_PROT
-					+ TucanMobile.TUCAN_HOST + scrape.registerLink.get(position),
-					localCookieManager, RequestObject.METHOD_GET, "");
-			callOverviewBrowser.execute(callstatuschange);
-		}
-	}
+    }
 
-	@Override
-	public ConfigurationChangeStorage saveConfiguration() {
-		ConfigurationChangeStorage cStore = new ConfigurationChangeStorage();
-		cStore.adapters.add(getListAdapter());
-		cStore.addScraper(scrape);
-		return cStore;
-	}
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        if (scrape.examSelection.get(position) == 2 || scrape.examSelection.get(position) == 3) {
+            if (scrape.examSelection.get(position) == 2)
+                mode = 1;
+            else if (scrape.examSelection.get(position) == 3)
+                mode = 1;
+            SimpleSecureBrowser callOverviewBrowser = new SimpleSecureBrowser(this);
+            RequestObject callstatuschange = new RequestObject(TucanMobile.TUCAN_PROT
+                    + TucanMobile.TUCAN_HOST + scrape.registerLink.get(position),
+                    localCookieManager, RequestObject.METHOD_GET, "");
+            callOverviewBrowser.execute(callstatuschange);
+        }
+    }
 
-	@Override
-	public void retainConfiguration(ConfigurationChangeStorage conf) {
-		setListAdapter(conf.adapters.get(0));
-		BasicScraper retainedScraper = conf.getScraper(0, this);
-		if (retainedScraper instanceof RegisterExamsScraper) {
-			scrape = (RegisterExamsScraper) retainedScraper;
-		}
-	}
+    @Override
+    public ConfigurationChangeStorage saveConfiguration() {
+        ConfigurationChangeStorage cStore = new ConfigurationChangeStorage();
+        cStore.adapters.add(getListAdapter());
+        cStore.addScraper(scrape);
+        return cStore;
+    }
+
+    @Override
+    public void retainConfiguration(ConfigurationChangeStorage conf) {
+        setListAdapter(conf.adapters.get(0));
+        BasicScraper retainedScraper = conf.getScraper(0, this);
+        if (retainedScraper instanceof RegisterExamsScraper) {
+            scrape = (RegisterExamsScraper) retainedScraper;
+        }
+    }
 
 }
