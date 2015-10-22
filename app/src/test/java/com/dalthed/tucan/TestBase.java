e8def6c69550f3129f5b5b05fab13e9e5131e778
Daniel Thiem <info@daniel-thiem.de>
2015-10-22 17:21:01 +0200

    Testdebug not failing

--- app/src/test/java/com/dalthed/tucan/TestBase.java
+++ app/src/test/java/com/dalthed/tucan/TestBase.java
@@ -2,9 +2,11 @@ package com.dalthed.tucan;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -40,5 +42,10 @@ public class TestBase {
             i++;
         } while (hasFoundResource);
 
+    }
+
+    @Test
+    public void doMockTest() {
+        assert true;
     }
 }
